28ddbe7a5c2cd4e655193d0dc358a6e837446333
Nathan Pabst <nathan.pabst@gmail.com>
2019-03-01 19:50:02 -0600

    added new classes with methods to compute stats

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,11 +8,34 @@ namespace Grades
 {
     class GradeBook
     {
+        //constructor
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+        }
+
+        //method to add a grade
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades;
+        //field to store a list of grades
+        private List<float> grades;
     }
 }
